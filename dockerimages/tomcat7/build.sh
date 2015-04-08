3f2bec2fb3f0cf6b17e3c764e70d8175dfd2e9d9
Christophe Delaitre <cdelaitre@gmail.com>
2015-04-08 10:42:40 +0200

    Update build.sh

--- dockerimages/tomcat7/build.sh
+++ dockerimages/tomcat7/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget http://wwwftp.ciril.fr/pub/apache/tomcat/tomcat-7/v7.0.59/bin/apache-tomcat-7.0.59.tar.gz
-docker build --rm=true -t "cdelaitre/tomcat7" .
+docker build --rm=true -t "cdelaitre/tomcat7" . |& tee build.log
