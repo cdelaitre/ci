2f8d8af9173f4bbbe29809b08d45942ba912f56f
cdelaitre <cdelaitre@gmail.com>
2015-04-09 17:35:52 +0200

    local build

--- dockerimages/tomcat7/build.sh
+++ dockerimages/tomcat7/build.sh
@@ -4,4 +4,8 @@ TAG=centos-tomcat:7
 LOG=/tmp/build-${TAG}.log
 FILE=apache-tomcat-7.0.59.tar.gz
 curl http://apache.mirrors.ovh.net/ftp.apache.org/dist/tomcat/tomcat-7/v7.0.59/bin/${FILE} -o ${FILE}
+if [ $? -ne 0 ]; then
+  echo "ERROR: unable to download [$URL]"
+  exit 1
+fi
 docker build --rm=true -t "${NAME}/${TAG}" . |& tee ${LOG}
--- dockerimages/tomcat8/run.sh
+++ dockerimages/tomcat8/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -it -p 52008:8080 cdelaitre/tomcat8
+docker run -d -it -p 52008:8080 cdelaitre/centos-tomcat:8
