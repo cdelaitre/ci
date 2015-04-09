628fb3b59f84e401fa92b185fd50738715bda879
cdelaitre <cdelaitre@gmail.com>
2015-04-09 17:07:19 +0200

    local build

--- dockerimages/tomcat7/build.sh
+++ dockerimages/tomcat7/build.sh
@@ -2,4 +2,6 @@
 NAME=cdelaitre
 TAG=centos-tomcat:7
 LOG=/tmp/build-${TAG}.log
+FILE=apache-tomcat-7.0.59.tar.gz
+curl http://apache.mirrors.ovh.net/ftp.apache.org/dist/tomcat/tomcat-7/v7.0.59/bin/${FILE} -o ${FILE}
 docker build --rm=true -t "${NAME}/${TAG}" . |& tee ${LOG}
